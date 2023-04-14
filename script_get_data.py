118be14af0824e9fcab7874f75e62f1faf4c5c1a
Axcel Espinoza <62627084+Axcel17@users.noreply.github.com>
2023-04-14 05:42:33 -0500

    Update script_get_data.py

--- script_get_data.py
+++ script_get_data.py
@@ -19,37 +19,32 @@ header = ['avgtemp_c','avghumidity','uv','visible','ir','soil_moisture', 'soil_m
 
 def main():
     while True:
-        # If the WHITE button was pressed then
-        # the led will CONTINUE OFF and and all data will be DELETED from the file
+        # If the WHITE button was pressed then the led will CONTINUE OFF and and all data will be DELETED from the file.
         if button_reset.is_pressed():
             with open('data.csv', 'w', newline='') as file:
                 file.truncate()
         
-        # If the GREEN button was pressed then
-        # the led will LIGHT UP and the data is being SAVED correctly
+        # If the GREEN button was pressed then the led will LIGHT UP and the data is being SAVED correctly.
         elif button_write.is_pressed():
             with open('data.csv', 'r+', newline='') as file:
                 reader = csv.reader(file)
                 writer = csv.writer(file)
                 
-                # If the file was EMPTY then the header is written 
+                # If the file was EMPTY then the header is written.
                 if [] == [row for row in reader]:
                     writer.writerow(header)
                 
-                # Switch at convenience. max_count: max number of iterations
+                # Switch at convenience. max_count: max number of iterations.
                 initial_count = 0
                 max_count = 50
                 
                 while initial_count < max_count:
-
                     # DHT sensor
                     humidity, temperature = dht_sensor.read()
-                    
                     # Sunlight sensor
                     visible = sunlight_sensor.ReadVisible
                     UV = sunlight_sensor.ReadUV/100 
                     IR = sunlight_sensor.ReadIR
-                    
                     # Moisture sensor
                     soil_moisture = moisture_sensor.moisture
                     if 0 <= soil_moisture and soil_moisture < 300:
@@ -58,13 +53,13 @@ def main():
                         soil_moisture_status = 'moist'
                     else:
                         soil_moisture_status = 'wet'
-                    
-                    # Get current date
+                    # Get current date.
                     time_now = datetime.datetime.now().strftime("%d/%m/%Y")
                     
-                    # Write data in CSV file
+                    # Write data in CSV file.
                     writer.writerow([temperature, humidity, UV, visible, IR, soil_moisture, soil_moisture_status, time_now])
-                    print([temperature, humidity, UV, visible, IR, soil_moisture, soil_moisture_status, time_now])                    
+                    #print([temperature, humidity, UV, visible, IR, soil_moisture, soil_moisture_status, time_now]) 
+                    
                     # Timeout between each iteration
                     time.sleep(0.25)
                     
@@ -72,14 +67,3 @@ def main():
 
 if __name__  == '__main__':
     main()
-    
-    
-# Referencias
-# DHT sensor
-# https://wiki.seeedstudio.com/Grove-TemperatureAndHumidity_Sensor/
-# Sunlight sensor
-# https://wiki.seeedstudio.com/Grove-Sunlight_Sensor/
-# Capacitive Moisture Sensor (Corrosion Resistant)
-# https://wiki.seeedstudio.com/Grove-Capacitive_Moisture_Sensor-Corrosion-Resistant/
-# Dual Button
-# https://wiki.seeedstudio.com/Grove-Dual-Button/
